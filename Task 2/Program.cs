244fa45eca72584b5b3407c6c302d3f76464fe97
Meruyert <kaliaskarova.mika@gmail.com>
2022-12-03 21:37:08 +0600

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+int max = a;
+int min = b;
+if (a > b) max = a; min = b;
+
+if (b > a) max = b; min = a;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
