9e0bcf8a8899ceab8fa0fdb37b73623f4a27b026
Meruyert <kaliaskarova.mika@gmail.com>
2022-12-03 19:34:36 +0600

    Задача №8

---
+++ Task 8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task 8
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = (0) * n;
+while (i <= n)
+{
+    Console.Write($"{i} ");
+    i++;
+}
