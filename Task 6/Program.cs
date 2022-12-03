e9c271195abaca5fbc4e6d9e689f07a0a368403b
Meruyert <kaliaskarova.mika@gmail.com>
2022-12-03 20:33:53 +0600

    Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int year = Convert.ToInt32(Console.ReadLine());
+
+if (year % 2 == 0)
+    Console.WriteLine("четное");
+else
+    Console.WriteLine("нечетное");
