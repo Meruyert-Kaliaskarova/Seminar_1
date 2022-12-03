793cee442090b77c251a264311fafea0947ac995
Meruyert <kaliaskarova.mika@gmail.com>
2022-12-03 21:20:28 +0600

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (a > b && a > c) max = a;
+
+if (b > a && b > c) max = b;
+
+if (c > a && c > b) max = c;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
